b5350d19685116f83e15f5afe040b128eb7cd87b
bala-amypo <bala@amypo.in>
2025-12-21 17:25:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public User createUser(User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email must be unique");
+        }
+
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        if (user.getRole() == null) {
+            user.setRole("ANALYST");
+        }
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+
+        User existing = getUser(id);
+
+        if (!existing.getEmail().equals(user.getEmail())
+                && userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email must be unique");
+        }
+
+        existing.setName(user.getName());
+        existing.setEmail(user.getEmail());
+
+        if (user.getPassword() != null) {
+            existing.setPassword(encoder.encode(user.getPassword()));
+        }
+
+        existing.setRole(user.getRole());
+
+        return userRepository.save(existing);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        getUser(id);
+        userRepository.deleteById(id);
+    }
+}

d5a0a128f2e93f70876e3ff13835723a89405a64
bala-amypo <bala@amypo.in>
2025-12-22 08:46:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/GeneratedShiftSchedule.java
+++ src/main/java/com/example/demo/entity/GeneratedShiftSchedule.java
@@ -1,12 +1,17 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Entity
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "generated_shift_schedule")
 public class GeneratedShiftSchedule {
 
     @Id
@@ -14,6 +19,8 @@ public class GeneratedShiftSchedule {
     private Long id;
 
     private Long employeeId;
+
     private Long shiftTemplateId;
-    private String date;
+
+    private LocalDate date;
 }

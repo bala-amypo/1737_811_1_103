9651b7d6c6d31e7381890ebae4688c9ddb1471c3
bala-amypo <bala@amypo.in>
2025-12-26 16:28:07 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,52 +1,23 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "email")
-})
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false)
     private String password;
+    private String role;
 
-    @Column(nullable = false)
-    private String role = "ANALYST";
+    public User() {}
 
-    public User() {
-    }
-
-    public User(Long id, String name, String email, String password, String role) {
-        this.id = id;
-        this.name = name;
+    public User(String email, String password, String role) {
         this.email = email;
         this.password = password;
-        this.role = (role == null) ? "ANALYST" : role;
+        this.role = role;
     }
 
-    // Getters & Setters
-
+    // getters and setters
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public void setId(Long id) {
@@ -56,23 +27,23 @@ public class User {
     public String getEmail() {
         return email;
     }
- 
+
     public void setEmail(String email) {
         this.email = email;
     }
- 
+
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
- 
+
     public String getRole() {
         return role;
     }
- 
+
     public void setRole(String role) {
         this.role = role;
     }

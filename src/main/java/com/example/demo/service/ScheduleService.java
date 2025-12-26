ae76e001695a46489fcecfaa04407967dae26ccc
bala-amypo <bala@amypo.in>
2025-12-26 16:39:12 +0000

    User commit

--- src/main/java/com/example/demo/service/ScheduleService.java
+++ src/main/java/com/example/demo/service/ScheduleService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.GeneratedShiftSchedule;
+import com.example.demo.model.GeneratedShiftSchedule;
 import java.util.List;
 
 public interface ScheduleService {
